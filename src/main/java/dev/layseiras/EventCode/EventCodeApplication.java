2480a3cc69d4b1ff67af0d015192e517a37540cd
Laysa <laysa.developer@gmail.com>
2025-02-18 21:00:27 -0300

    first commit

---
+++ src/main/java/dev/layseiras/EventCode/EventCodeApplication.java
@@ -0,0 +1,13 @@
+package dev.layseiras.EventCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/layseiras/EventCode/EventCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.layseiras.EventCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
