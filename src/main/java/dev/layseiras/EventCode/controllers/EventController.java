29ae929d68d86b349a8159df11db5d867e72f3ae
Laysa <laysa.developer@gmail.com>
2025-02-19 21:20:02 -0300

    feat: add post and get routes for events with specific http status body

---
+++ src/main/java/dev/layseiras/EventCode/controllers/EventController.java
@@ -0,0 +1,35 @@
+package dev.layseiras.EventCode.controllers;
+
+import dev.layseiras.EventCode.entities.Event;
+import dev.layseiras.EventCode.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EventController {
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping("/events")
+    public Event addNewEvent(@RequestBody Event newEvent){
+        return eventService.addNewEvent(newEvent);
+    }
+
+    @GetMapping("/events")
+    public List<Event> getAllEvents(){
+        return eventService.getAllEvents();
+    }
+
+    @GetMapping("/events/{prettyName}")
+    public ResponseEntity<Event> getEventByPrettyName(@PathVariable String prettyName){
+        Event evt = eventService.getEventByPrettyName(prettyName);
+        if(evt != null){
+            return ResponseEntity.ok().body(evt);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
