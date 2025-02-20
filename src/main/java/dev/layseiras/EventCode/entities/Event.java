65e32fcde8c0afa02ce62501ceae3831a08c4bb9
Laysa <laysa.developer@gmail.com>
2025-02-19 21:12:22 -0300

    feat: add Event model with jpa annotations

---
+++ src/main/java/dev/layseiras/EventCode/entities/Event.java
@@ -0,0 +1,125 @@
+package dev.layseiras.EventCode.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_event")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id")
+    private Long id;
+
+    @Column(name = "name", length = 255, nullable = false)
+    private String name;
+
+    @Column(name = "pretty_name", length = 50, nullable = false, unique = true)
+    private String prettyName;
+
+    @Column(name = "location", length = 255, nullable = false)
+    private String location;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @Column(name = "start_time")
+    private LocalDate startTime;
+
+    @Column(name = "end_time")
+    private LocalDate endTime;
+
+    public Event() {
+    }
+
+    public Event(Long id, String name, String prettyName, String location, Double price, LocalDate startDate, LocalDate endDate, LocalDate startTime, LocalDate endTime) {
+        this.id = id;
+        this.name = name;
+        this.prettyName = prettyName;
+        this.location = location;
+        this.price = price;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrettyName() {
+        return prettyName;
+    }
+
+    public void setPrettyName(String prettyName) {
+        this.prettyName = prettyName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public LocalDate getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDate startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDate getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDate endTime) {
+        this.endTime = endTime;
+    }
+}
