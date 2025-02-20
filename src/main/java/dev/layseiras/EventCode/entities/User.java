7737742382fbc12fa839253f45c593491049dbab
Laysa <laysa.developer@gmail.com>
2025-02-19 21:44:22 -0300

    feat: add user model with jpa annotations

---
+++ src/main/java/dev/layseiras/EventCode/entities/User.java
@@ -0,0 +1,49 @@
+package dev.layseiras.EventCode.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    @Column(name = "username", length = 255, nullable = false)
+    private String userName;
+    @Column(name = "email", length = 255, unique = true, nullable = false)
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String userName, String email) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
