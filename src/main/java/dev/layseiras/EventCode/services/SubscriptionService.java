687bb5378ad808f65b3578ba8f5705a17e802c2b
Layseiras <layseirasdev@outlook.com>
2025-02-22 09:36:45 -0300

    feat: create subscription service for add new subs

---
+++ src/main/java/dev/layseiras/EventCode/services/SubscriptionService.java
@@ -0,0 +1,34 @@
+package dev.layseiras.EventCode.services;
+
+import dev.layseiras.EventCode.entities.Event;
+import dev.layseiras.EventCode.entities.Subscription;
+import dev.layseiras.EventCode.entities.User;
+import dev.layseiras.EventCode.repositories.EventRepo;
+import dev.layseiras.EventCode.repositories.SubscriptionRepo;
+import dev.layseiras.EventCode.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubscriptionService {
+
+    @Autowired
+    private EventRepo evtRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private SubscriptionRepo subRepo;
+
+    public Subscription addNewSubscription(String eventName, User user){
+        Subscription subs = new Subscription();
+        Event evt = evtRepo.findByPrettyName(eventName);
+        user = userRepo.save(user);
+        subs.setEvent(evt);
+        subs.setSubscriber(user);
+
+        Subscription res = subRepo.save(subs);
+        return res;
+    }
+}
