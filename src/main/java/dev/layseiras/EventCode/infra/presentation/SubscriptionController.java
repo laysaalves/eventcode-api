e46e59f89a5dc7feaf9484042773f635120c7a0a
Layseiras <layseirasdev@outlook.com>
2025-02-22 10:55:50 -0300

    feat(controller): add EventNotFound exception to subscription

--- src/main/java/dev/layseiras/EventCode/infra/presentation/SubscriptionController.java
+++ src/main/java/dev/layseiras/EventCode/infra/presentation/SubscriptionController.java
@@ -3,6 +3,8 @@ package dev.layseiras.EventCode.infra.presentation;
 import dev.layseiras.EventCode.core.entities.Subscription;
 import dev.layseiras.EventCode.core.entities.User;
 import dev.layseiras.EventCode.core.service.SubscriptionService;
+import dev.layseiras.EventCode.infra.exception.EventNotFoundException;
+import org.springdoc.api.ErrorMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +18,14 @@ public class SubscriptionController {
     private SubscriptionService service;
 
     @PostMapping("/subscription/{prettyName}")
-    public ResponseEntity<Subscription> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber) {
-        Subscription res = service.addNewSubscription(prettyName, subscriber);
-        if (res != null) {
-            return ResponseEntity.ok(res);
+    public ResponseEntity<?> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber) {
+        try {
+            Subscription res = service.addNewSubscription(prettyName, subscriber);
+            if (res != null) {
+                return ResponseEntity.ok(res);
+            }
+        } catch (EventNotFoundException e) {
+            return ResponseEntity.status(404).body(new ErrorMessage(e.getMessage()));
         }
         return ResponseEntity.badRequest().build();
     }
